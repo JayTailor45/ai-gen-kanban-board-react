08ef0b4413f2b2d042dd38c3a73ceb66c625c34d
Jay Tailor <tailorj64@gmail.com>
2024-08-01 18:47:09 +0530

    improve styles

--- src/KanbanBoard.js
+++ src/KanbanBoard.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import './KanbanBoard.css';
 
 const KanbanBoard = () => {
     const [newTask, setNewTask] = useState('');
